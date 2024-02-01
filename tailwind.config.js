ca7ba7b3ec019f703a1fde48e4b2664ae7d761e1
AlexChabriel <alexandra.chabriel@gmail.com>
2024-02-01 18:31:54 +0100

    page register

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./assets/**/*.js",
     "./templates/**/*.html.twig",
   ],  theme: {
-    extend: {},
+    extend: {
+      colors: {
+        PrimaryGreen: '#A4AC86', // Replace this with your custom color code
+        SecondaryGreen: '#004225',
+        ThirdGreen: '#5DB075',
+      },
+      fontFamily: {
+        OpanSans: ['"Opan Sans"', "open-sans"],
+        lora: ['"lora"', "Lora"],
+      },
+    },
   },
   plugins: [],
 }
