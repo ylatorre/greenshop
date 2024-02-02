c2fab056c3978a6c618eea2c65968531fc96e073
Helya <97184337+HelyaC@users.noreply.github.com>
2024-02-02 17:09:35 +0100

    affichage produits

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -5,14 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Categorie;
 
 class CategorieController extends AbstractController
 {
     #[Route('/categorie', name: 'app_categorie')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $categorieRepository = $entityManager->getRepository(Categorie::class);
+        $categories = $categorieRepository->findAll();
+
         return $this->render('categorie/index.html.twig', [
             'controller_name' => 'CategorieController',
+            'categories' => $categories
         ]);
     }
 }
