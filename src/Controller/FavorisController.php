ba146b18505189756d0f0f921cc1480aa0d879c4
camilntl <91890013+camilntl@users.noreply.github.com>
2024-03-01 14:50:32 +0100

    favoris

--- src/Controller/FavorisController.php
+++ src/Controller/FavorisController.php
@@ -6,7 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ListeRepository; // Assurez-vous d'importer le bon repository
-
+use App\Repository\FicheProduitRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class FavorisController extends AbstractController
 {
@@ -27,9 +28,39 @@ class FavorisController extends AbstractController
         return $this->render('favoris/index.html.twig', [
             'favoris' => $favoris,
         ]);
+
     }
+    #[Route('/ajouter-aux-favoris/{idProduit}', name: 'ajouter_aux_favoris')]
+    public function ajouterAuxFavoris($idProduit, FicheProduitRepository $produitRepository, EntityManagerInterface $entityManager, ListeRepository $listeRepository): Response
+    {
+        $user = $this->getUser();
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $produit = $produitRepository->find($idProduit);
+        if (!$produit) {
+            return $this->redirectToRoute('homepage');
+        }
+
+        // Trouver ou créer la liste de favoris pour l'utilisateur
+        $favoris = $listeRepository->findOneBy(['user' => $user, 'typeListe' => 'FAVORIS']);
+        if (!$favoris) {
+            // Assumer ici que Liste est votre entité représentant une liste et qu'elle a une méthode setTypeListe
+            $favoris = new Liste(); // Assurez-vous que c'est le bon nom d'entité
+            $favoris->setUser($user);
+            $favoris->setTypeListe('FAVORIS');
+            $entityManager->persist($favoris);
+        }
+
+        // Assumer que Liste a une méthode pour ajouter un produit aux favoris
+        $favoris->addFicheProduit($produit); // Adaptez cette ligne en fonction de votre implémentation
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_favoris'); // Utilisez le nom de la route
+    }
+}
 
 
 
     
-}
