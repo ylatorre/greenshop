11a9bbb839e816c1857c486df742a914a3c6322b
camilntl <91890013+camilntl@users.noreply.github.com>
2024-03-01 13:08:01 +0100

    s

--- src/Entity/Photo.php
+++ src/Entity/Photo.php
@@ -4,6 +4,9 @@ namespace App\Entity;
 
 use App\Repository\PhotoRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich; 
+
 
 #[ORM\Entity(repositoryClass: PhotoRepository::class)]
 class Photo
@@ -30,7 +33,7 @@ class Photo
         return $this->image;
     }
 
-    public function setImage(string $image): static
+    public function setImage(string $image): self
     {
         $this->image = $image;
 
@@ -55,6 +58,7 @@ class Photo
 
     public function setImageFile(?File $imageFile = null): void
     {
+        
         $this->imageFile = $imageFile;
         if (null !== $imageFile) {
             // Il est nécessaire de mettre à jour l'entité pour que la date de mise à jour soit modifiée
